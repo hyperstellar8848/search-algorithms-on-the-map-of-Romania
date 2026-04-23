fd57bbe002002a55b72c4162ea7bb81bf3e947d8
Parmida Beiki <hyperstellar11034@gmail.com>
2026-04-23 18:03:59 +0330

    Create graph.py

---
+++ graph.py
@@ -0,0 +1,23 @@
+# Adjacency List (list of tuples)
+
+graph = {
+    "Arad": [("Zerind", 75), ("Sibiu", 140), ("Timisoara", 118)],
+    "Bucharest": [("Pitesti", 101), ("Fagaras", 211), ("Urziceni", 85), ("Giurgiu", 90)],
+    "Craiova": [("Drobeta", 120), ("Rimnicu Vilcea", 146), ("Pitesti", 138)],
+    "Drobeta": [("Craiova", 120), ("Mehadia", 75)],
+    "Eforie": [("Hirsova", 86)],
+    "Fagaras": [("Sibiu", 99), ("Bucharest", 211)],
+    "Giurgiu": [("Bucharest", 90)],
+    "Hirsova": [("Vaslui", 142), ("Urziceni", 98), ("Eforie", 86)],
+    "Iasi": [("Neamt", 87), ("Vaslui", 92)],
+    "Lugoj": [("Timisoara", 111), ("Mehadia", 70)],
+    "Mehadia": [("Lugoj", 70), ("Drobeta", 75)],
+    "Neamt": [("Iasi", 87)],
+    "Oradea": [("Zerind", 71), ("Sibiu", 151)],
+    "Pitesti": [("Rimnicu Vilcea", 97), ("Craiova", 138), ("Bucharest", 101)],
+    "Rimnicu Vilcea": [("Sibiu", 80), ("Pitesti", 97), ("Craiova", 146)],
+    "Sibiu": [("Arad", 140), ("Fagaras", 99), ("Rimnicu Vilcea", 80), ("Oradea", 151)],
+    "Timisoara": [("Arad", 118), ("Lugoj", 111)],
+    "Urziceni": [("Bucharest", 85), ("Hirsova", 98), ("Vaslui", 142)],
+    "Vaslui": [("Iasi", 92), ("Urziceni", 142), ("Hirsova", 142)],
+}
